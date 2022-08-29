0d92850ee91cf76219368b857a5087a59c1aec7c
SkiesGgw <skies.ggw@gmail.com>
2022-08-29 14:06:22 +0800

    init

---
+++ bedao-en.js
@@ -0,0 +1,25 @@
+module.exports = {
+  nav:{
+    text1: "Treasury",
+    text2: "Token",
+    text3: "Analysis",
+    text4: "Documentation",
+    text5: "Governance",
+    text6: "Forum",
+  },
+  home: {
+    text1: "TOTAL AMOUNT OF",
+    text2: "THE MINING POOL",
+    text3: "BeDAO is a project dedicated to following and incubating web3.0.",
+    text4: "And a decentralized autonomous organization that gathers highly consensus users.",
+    text5: "JOIN OUR DISCORD",
+    text6: "STAKING MINING",
+  },
+  footer: {
+    text1: "FuniBet",
+    text2: "Privacy Policy",
+    text3: "Cookies",
+    text4: "Terms & Conditions",
+    text5: "Contact Us",
+  }
+}
